b61bc58ac2e5801d172d2015fd417333c33dec3d
Lenko27 <lenkov-nick@mail.ru>
2023-10-27 19:34:54 +0300

    second fix

---
+++ src/Lab1/Tools/SpaceBuilders/DefaultSpaceBuilder.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections.Generic;
+using Itmo.ObjectOrientedProgramming.Lab1.Interfaces;
+using Itmo.ObjectOrientedProgramming.Lab1.Models.Obstacles;
+using Itmo.ObjectOrientedProgramming.Lab1.Models.SpaceTypes;
+
+namespace Itmo.ObjectOrientedProgramming.Lab1.Tools.SpaceBuilders;
+
+public class DefaultSpaceBuilder
+{
+    private readonly List<IObstacle> _obstacles;
+
+    public DefaultSpaceBuilder()
+    {
+        _obstacles = new List<IObstacle>();
+    }
+
+    public DefaultSpaceBuilder AddObstacle(Asteroid obstacle)
+    {
+        _obstacles.Add(obstacle);
+        return this;
+    }
+
+    public DefaultSpaceBuilder AddObstacle(Meteorite obstacle)
+    {
+        _obstacles.Add(obstacle);
+        return this;
+    }
+
+    public DefaultSpace Build()
+    {
+        return new DefaultSpace(_obstacles);
+    }
+}
