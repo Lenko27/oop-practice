65e46c9e2eade0eab270378e7544c792070f9a06
Lenko27 <lenkov-nick@mail.ru>
2023-10-27 14:27:42 +0300

    done

---
+++ src/Lab2/Infrastructure/ComputerComponentsRepository.cs
@@ -0,0 +1,110 @@
+﻿using System;
+using System.Collections.Generic;
+using Itmo.ObjectOrientedProgramming.Lab2.Models;
+
+namespace Itmo.ObjectOrientedProgramming.Lab2.Infrastructure;
+
+public static class ComputerComponentsRepository
+{
+    private static readonly List<Motherboard> _motherboards = new();
+    private static readonly List<Cpu> _cpus = new();
+    private static readonly List<CpuCoolingSystem> _coolingSystems = new();
+    private static readonly List<Gpu> _gpus = new();
+    private static readonly List<ComputerCase> _computerCases = new();
+    private static readonly List<PowerSupply> _powerSupplies = new();
+    private static readonly List<WiFiAdapter> _wifiAdapters = new();
+    private static readonly List<SsdDrive> _ssdDrives = new();
+    private static readonly List<HardDisk> _hardDisks = new();
+    private static readonly List<Ram> _rams = new();
+
+    public static IReadOnlyList<Motherboard> Motherboards => _motherboards;
+    public static IReadOnlyList<Cpu> Cpus => _cpus;
+    public static IReadOnlyList<CpuCoolingSystem> CoolingSystems => _coolingSystems;
+    public static IReadOnlyList<Gpu> Gpus => _gpus;
+    public static IReadOnlyList<ComputerCase> ComputerCases => _computerCases;
+    public static IReadOnlyList<PowerSupply> PowerSupplies => _powerSupplies;
+    public static IReadOnlyList<WiFiAdapter> WiFiAdapters => _wifiAdapters;
+    public static IReadOnlyList<SsdDrive> SsdDrives => _ssdDrives;
+    public static IReadOnlyList<HardDisk> HardDisks => _hardDisks;
+    public static IReadOnlyList<Ram> Rams => _rams;
+
+    public static void AddMotherboard(Motherboard motherboard)
+    {
+        ArgumentNullException.ThrowIfNull(motherboard, nameof(motherboard));
+
+        if (!_motherboards.Contains(motherboard))
+            _motherboards.Add(motherboard);
+    }
+
+    public static void AddCpu(Cpu cpu)
+    {
+        ArgumentNullException.ThrowIfNull(cpu, nameof(cpu));
+
+        if (!_cpus.Contains(cpu))
+            _cpus.Add(cpu);
+    }
+
+    public static void AddCoolingSystem(CpuCoolingSystem coolingSystem)
+    {
+        ArgumentNullException.ThrowIfNull(coolingSystem, nameof(coolingSystem));
+
+        if (!_coolingSystems.Contains(coolingSystem))
+            _coolingSystems.Add(coolingSystem);
+    }
+
+    public static void AddGpu(Gpu gpu)
+    {
+        ArgumentNullException.ThrowIfNull(gpu, nameof(gpu));
+
+        if (!_gpus.Contains(gpu))
+            _gpus.Add(gpu);
+    }
+
+    public static void AddComputerCase(ComputerCase computerCase)
+    {
+        ArgumentNullException.ThrowIfNull(computerCase, nameof(computerCase));
+
+        if (!_computerCases.Contains(computerCase))
+            _computerCases.Add(computerCase);
+    }
+
+    public static void AddPowerSupply(PowerSupply powerSupply)
+    {
+        ArgumentNullException.ThrowIfNull(powerSupply, nameof(powerSupply));
+
+        if (!_powerSupplies.Contains(powerSupply))
+            _powerSupplies.Add(powerSupply);
+    }
+
+    public static void AddWiFiAdapter(WiFiAdapter wifiAdapter)
+    {
+        ArgumentNullException.ThrowIfNull(wifiAdapter, nameof(wifiAdapter));
+
+        if (!_wifiAdapters.Contains(wifiAdapter))
+            _wifiAdapters.Add(wifiAdapter);
+    }
+
+    public static void AddSsdDrive(SsdDrive ssd)
+    {
+        ArgumentNullException.ThrowIfNull(ssd, nameof(ssd));
+
+        if (!_ssdDrives.Contains(ssd))
+            _ssdDrives.Add(ssd);
+    }
+
+    public static void AddHardDisk(HardDisk hardDisk)
+    {
+        ArgumentNullException.ThrowIfNull(hardDisk, nameof(hardDisk));
+
+        if (!_hardDisks.Contains(hardDisk))
+            _hardDisks.Add(hardDisk);
+    }
+
+    public static void AddRam(Ram ram)
+    {
+        ArgumentNullException.ThrowIfNull(ram, nameof(ram));
+
+        if (!_rams.Contains(ram))
+            _rams.Add(ram);
+    }
+}
---
+++ src/Lab2/Models/Computer.cs
@@ -0,0 +1,150 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using Itmo.ObjectOrientedProgramming.Lab2.Exceptions;
+using Itmo.ObjectOrientedProgramming.Lab2.Infrastructure;
+
+namespace Itmo.ObjectOrientedProgramming.Lab2.Models;
+
+public class Computer
+{
+    private readonly List<SsdDrive> _ssdDrives;
+    private readonly List<HardDisk> _hardDisks;
+    private readonly List<Ram> _rams;
+
+    private Motherboard? _motherboard;
+    private Cpu? _cpu;
+    private CpuCoolingSystem? _coolingSystem;
+    private Gpu? _gpu;
+    private ComputerCase? _case;
+    private PowerSupply? _powerSupply;
+    private WiFiAdapter? _wifiAdapter;
+
+    public Computer()
+    {
+        _ssdDrives = new();
+        _hardDisks = new();
+        _rams = new();
+    }
+
+    public Motherboard? Motherboard
+    {
+        get => _motherboard;
+        set
+        {
+            if (!ComputerComponentsRepository.Motherboards.Contains(value))
+                throw new ComponentNotRegisteredException();
+
+            _motherboard = value;
+        }
+    }
+
+    public Cpu? Cpu
+    {
+        get => _cpu;
+        set
+        {
+            if (!ComputerComponentsRepository.Cpus.Contains(value))
+                throw new ComponentNotRegisteredException();
+
+            _cpu = value;
+        }
+    }
+
+    public CpuCoolingSystem? CoolingSystem
+    {
+        get => _coolingSystem;
+        set
+        {
+            if (!ComputerComponentsRepository.CoolingSystems.Contains(value))
+                throw new ComponentNotRegisteredException();
+
+            _coolingSystem = value;
+        }
+    }
+
+    public Gpu? Gpu
+    {
+        get => _gpu;
+        set
+        {
+            if (!ComputerComponentsRepository.Gpus.Contains(value))
+                throw new ComponentNotRegisteredException();
+
+            _gpu = value;
+        }
+    }
+
+    public ComputerCase? Case
+    {
+        get => _case;
+        set
+        {
+            if (!ComputerComponentsRepository.ComputerCases.Contains(value))
+                throw new ComponentNotRegisteredException();
+
+            _case = value;
+        }
+    }
+
+    public PowerSupply? PowerSupply
+    {
+        get => _powerSupply;
+        set
+        {
+            if (!ComputerComponentsRepository.PowerSupplies.Contains(value))
+                throw new ComponentNotRegisteredException();
+
+            _powerSupply = value;
+        }
+    }
+
+    public WiFiAdapter? WiFiAdapter
+    {
+        get => _wifiAdapter;
+        set
+        {
+            if (!ComputerComponentsRepository.WiFiAdapters.Contains(value))
+                throw new ComponentNotRegisteredException();
+
+            _wifiAdapter = value;
+        }
+    }
+
+    public IReadOnlyList<SsdDrive> SsdDrives => _ssdDrives;
+    public IReadOnlyList<HardDisk> HardDisks => _hardDisks;
+    public IReadOnlyList<Ram> Rams => _rams;
+
+    public void AddSsdDrive(SsdDrive ssd)
+    {
+        ArgumentNullException.ThrowIfNull(ssd, nameof(ssd));
+
+        if (!ComputerComponentsRepository.SsdDrives.Contains(ssd))
+            throw new ComponentNotRegisteredException();
+
+        if (!_ssdDrives.Contains(ssd))
+            _ssdDrives.Add(ssd);
+    }
+
+    public void AddHardDisk(HardDisk hardDisk)
+    {
+        ArgumentNullException.ThrowIfNull(hardDisk, nameof(hardDisk));
+
+        if (!ComputerComponentsRepository.HardDisks.Contains(hardDisk))
+            throw new ComponentNotRegisteredException();
+
+        if (!_hardDisks.Contains(hardDisk))
+            _hardDisks.Add(hardDisk);
+    }
+
+    public void AddRam(Ram ram)
+    {
+        ArgumentNullException.ThrowIfNull(ram, nameof(ram));
+
+        if (!ComputerComponentsRepository.Rams.Contains(ram))
+            throw new ComponentNotRegisteredException();
+
+        if (!_rams.Contains(ram))
+            _rams.Add(ram);
+    }
+}
